db61bb5d0783d66bcbf0486afaa4cb251a12c6de
hmwang <772207511@qq.com>
2021-09-17 22:14:44 +0800

    version1.0

--- src/main/java/com/cisco/webex/day002/ServerSocketDemo01.java
+++ src/main/java/com/cisco/webex/day002/ServerSocketDemo01.java
@@ -1,10 +1,29 @@
 package com.cisco.webex.day002;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 /**
  * @program: Netty-demo
  * @description: 单线程阻塞处理多个client请求
  * @author: hemwang
  * @create: 2021-09-17 22:02
  **/
+@Slf4j
 public class ServerSocketDemo01 {
+
+    public static void main(String[] args) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(16);
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.bind(new InetSocketAddress(8888));
+        while(true){
+            SocketChannel sc = ssc.accept();
+
+        }
+    }
 }
