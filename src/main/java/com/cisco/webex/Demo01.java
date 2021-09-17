b11ac6899060e4764f8544072f00bb0de2c276ce
hmwang <772207511@qq.com>
2021-09-17 21:48:29 +0800

    Initial commit

---
+++ src/main/java/com/cisco/webex/Demo01.java
@@ -0,0 +1,41 @@
+package com.cisco.webex;
+
+
+
+import lombok.extern.slf4j.Slf4j;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @program: Netty-demo
+ * @description: 测试Java-NIO
+ * @author: hemwang
+ * @create: 2021-09-14 22:06
+ **/
+
+public class Demo01 {
+    public static void main(String[] args){
+
+        // 1，创建输入流
+        try(FileChannel channel =  new FileInputStream("word.txt").getChannel()) {
+            // 2，构建buffer
+            ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+            int len = 0;
+            // 3，从输入流中读取数据到buffer
+            do {
+                len = channel.read(byteBuffer);
+                // 4，将buffer设置成可读状态
+                byteBuffer.flip();
+                while (byteBuffer.hasRemaining()) {
+                    System.out.println((char) byteBuffer.get());
+                }
+                // 5，将buffer设置成可写状态
+                byteBuffer.clear();
+            }while(len != -1);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/cisco/webex/Demo02.java
@@ -0,0 +1,36 @@
+package com.cisco.webex;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @program: Netty-demo
+ * @description: 底层如何处理粘包半包的
+ * @author: hemwang
+ * @create: 2021-09-15 21:58
+ **/
+public class Demo02 {
+    public static void main(String[] args) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(32);
+        byteBuffer.put("hello world\nhemwang\nde".getBytes());
+        split(byteBuffer);
+        byteBuffer.put("ng ya\n".getBytes());
+        split(byteBuffer);
+    }
+
+    private static void split(ByteBuffer byteBuffer) {
+        byteBuffer.flip();
+        for (int i = 0; i < byteBuffer.limit(); i++) {
+            if(byteBuffer.get(i) == '\n'){
+                int length = i - byteBuffer.position() + 1;
+                ByteBuffer b1 = ByteBuffer.allocate(length);
+                for (int j = 0; j < length; j++) {
+                    b1.put(byteBuffer.get());
+                }
+                b1.flip();
+                System.out.println(StandardCharsets.UTF_8.decode(b1));
+            }
+        }
+        byteBuffer.compact();
+    }
+}
